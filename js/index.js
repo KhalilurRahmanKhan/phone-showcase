c294626e73d02cf640b07e6fddced2796f95410b
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-03-01 13:07:05 +0600

    details function done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function getSearchInput(){
+const getSearchInput = () => {
     document.getElementById("search").classList.remove("search");
     const searchText = document.getElementById("search").value;
     if(searchText.length > 0){
@@ -15,7 +15,7 @@ function getSearchInput(){
     document.getElementById("search").value = "";
 }
 
-function loadPhones(phones){
+const loadPhones = phones => {
     const showcase = document.getElementById("showcase");
     showcase.textContent = "";
     
@@ -35,7 +35,7 @@ function loadPhones(phones){
                 <div class="card-body">
                 <h5 class="card-title">${phone.brand}</h5>
                 <p class="card-text">${phone.phone_name}</p>
-                <a href="#" class="btn btn-primary">Show Details</a>
+                <a  onclick="getDetails(${phone.slug})" class="btn btn-primary">Show Details</a>
                 </div>
             </div>
             `;
@@ -54,7 +54,7 @@ function loadPhones(phones){
                     <div class="card-body">
                     <h5 class="card-title">${phones[i].brand}</h5>
                     <p class="card-text">${phones[i].phone_name}</p>
-                    <a href="#" class="btn btn-primary">Show Details</a>
+                    <a  onclick="getDetails('${phones[i].slug}')" class="btn btn-primary">Show Details</a>
                     </div>
                 </div>
                 `;
@@ -67,3 +67,43 @@ function loadPhones(phones){
         
     }
     }
+
+
+const getDetails = slug => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        .then(response => response.json())
+        .then(data => loadDetails(data) );
+
+}
+
+const loadDetails = data => {
+    const details = document.getElementById("details");
+    details.textContent = "";
+
+
+    const div = document.createElement('div');
+    div.classList.add("details");
+    div.innerHTML = `
+    <img class="d-block mx-auto mb-4" src="${data.data.image}" alt="" width="250" height="250">
+    <h4 class="">${data.data.name}</h4>
+    <p><b>Release date</b>: ${data.data.releaseDate ? data.data.releaseDate : "Release date not found" }</p>
+    <ul>
+        <li><b>Storage</b>: ${data.data.mainFeatures.storage}</li>
+        <li><b>Display size</b>: ${data.data.mainFeatures.displaySize}</li>
+        <li><b>Chipset</b>: ${data.data.mainFeatures.chipSet}</li>
+        <li><b>Memory</b>: ${data.data.mainFeatures.memory}</li>
+    </ul>
+    <p><b>Sensors</b>: ${data.data.mainFeatures.sensors}</p>
+    <div style="border:1px solid grey; width:70%; margin:auto;"><b>Others</b>:
+    <ul>
+        <li><b>WLAN</b>: ${data.data.others.WLAN}</li>
+        <li><b>Bluetooth</b>: ${data.data.others.Bluetooth}</li>
+        <li><b>GPS</b>: ${data.data.others.GPS}</li>
+        <li><b>NFC</b>: ${data.data.others.NFC}</li>
+        <li><b>Radio</b>: ${data.data.others.Radio}</li>
+        <li><b>USB</b>: ${data.data.others.USB}</li>
+    </ul>
+    </div>
+    `;
+    details.appendChild(div);
+}
